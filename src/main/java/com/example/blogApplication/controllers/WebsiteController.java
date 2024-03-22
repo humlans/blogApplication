c96cf3f1468b3ccbccbba19c494601c0dd3bb338
humlans <spangbergwilma@gmail.com>
2024-03-22 11:21:22 +0100

    Base style implemented

--- src/main/java/com/example/blogApplication/controllers/WebsiteController.java
+++ src/main/java/com/example/blogApplication/controllers/WebsiteController.java
@@ -1,4 +1,14 @@
 package com.example.blogApplication.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class WebsiteController {
+
+    @GetMapping("homepage")
+    public String homepage(Model model){
+        return "homePage";
+    }
 }
