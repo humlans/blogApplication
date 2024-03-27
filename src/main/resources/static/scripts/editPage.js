3a75d232bcec6448cbfe50fa04233396b2aece9f
amso95 <amanda-solberg@hotmail.com>
2024-03-27 13:07:32 +0100

    GET-method to API implemented

--- src/main/resources/static/scripts/editPage.js
+++ src/main/resources/static/scripts/editPage.js
@@ -13,33 +13,21 @@ home.addEventListener("click",
 );
 
 function httpGet(){
-    /*var xmlHttp = new XMLHttpRequest();
-
-    xmlHttp.onreadystatechange = function() {
-            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-                callback(xmlHttp.responseText);
-                var y = JSON.parse(xmlHttp.responseText);
-                console.log(xmlHttp);
-                console.log(y);
-                document.getElementById("testP").innerHTML = y[1].id;;
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", "http://localhost:8080/blog-post?id=1");
+      xhr.send();
+      xhr.responseType = "json";
+      xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          console.log(xhr.response);
+          document.getElementById("testP").innerHTML = "<div style='margin-top:10px'><h2>" + xhr.response.title + "</h2>" +
+                                                        "<p>" + xhr.response.textContent + "</p>"+
+                                                        "<p>Date: " + xhr.response.date + ", id: " +
+                                                        xhr.response.id + ", userId: " + xhr.response.userId
+                                                        + "</p></div>";
         }
-        xmlHttp.open("GET", "http://localhost:8080/blog-post?id=1", true); // true for asynchronous
-        xmlHttp.send(null);
-
-    xmlHttp.open("GET", "http://localhost:8080/blog-post?id=1", false); // false for synchronous request
-    console.log(xmlHttp.open("GET", "http://localhost:8080/blog-post?id=1", false));
-    xmlHttp.send( null );
-    return xmlHttp.responseText;*/
-
-    const url = "http://localhost:8080/blog-post?id=1";
-    fetch(url)
-        .then(
-            response => response.json() // .text(), etc.
-            // same as function(response) {return response.json();}
-        ).then(
-            jsonString => {
-                const json = JSON.parse(jsonString);
-                document.getElementById("testP").innerHTML = json[1].id;
-            }
-        );
+        else {
+          console.log(`Error: ${xhr.status}`);
+        }
+    };
 }
