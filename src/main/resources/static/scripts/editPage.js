9c229d9eb1598eab7d3613088bb42fb2d6969b1c
amso95 <amanda-solberg@hotmail.com>
2024-03-27 11:54:06 +0100

    httpRequest try

--- src/main/resources/static/scripts/editPage.js
+++ src/main/resources/static/scripts/editPage.js
@@ -11,3 +11,35 @@ home.addEventListener("click",
         window.location = "homepage";
     }
 );
+
+function httpGet(){
+    /*var xmlHttp = new XMLHttpRequest();
+
+    xmlHttp.onreadystatechange = function() {
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                callback(xmlHttp.responseText);
+                var y = JSON.parse(xmlHttp.responseText);
+                console.log(xmlHttp);
+                console.log(y);
+                document.getElementById("testP").innerHTML = y[1].id;;
+        }
+        xmlHttp.open("GET", "http://localhost:8080/blog-post?id=1", true); // true for asynchronous
+        xmlHttp.send(null);
+
+    xmlHttp.open("GET", "http://localhost:8080/blog-post?id=1", false); // false for synchronous request
+    console.log(xmlHttp.open("GET", "http://localhost:8080/blog-post?id=1", false));
+    xmlHttp.send( null );
+    return xmlHttp.responseText;*/
+
+    const url = "http://localhost:8080/blog-post?id=1";
+    fetch(url)
+        .then(
+            response => response.json() // .text(), etc.
+            // same as function(response) {return response.json();}
+        ).then(
+            jsonString => {
+                const json = JSON.parse(jsonString);
+                document.getElementById("testP").innerHTML = json[1].id;
+            }
+        );
+}
